4620cfd2f64a0c9de54b84a6233ea276ea1b6768
Matthew Sharpe <matthew.sharpe@enable.com>
2022-07-04 09:30:04 +0100

    Add availability test endpoint

---
+++ src/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MonkeyCage.WebApi.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return NoContent();
+        }
+    }
+}
